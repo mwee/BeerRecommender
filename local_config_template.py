33077a1b795f9680ea8bbecd9b76c57d50554987
Michael Traver <mtraver@college.harvard.edu>
2013-12-12 21:00:07 -0500

    Add some comments to local_config_template.py describing what the file is and how to use it.

--- local_config_template.py
+++ local_config_template.py
@@ -1,4 +1,15 @@
+# This file is a template for the configuration that must be defined for the IPython notebook to run.
+# Make a copy of this file, rename it local_config.py, and fill in the file paths below. The notebook
+# should then know where to look for resources.
+
+# the path to the .csv file containing reviews
 REVIEWS_FILE_PATH = ''
+
+# the path to the .csv file containing beer data
 BEERS_FILE_PATH = ''
+
+# the path to the sqlite3 database file of beer similarities
 BEER_SIM_DB_FILE_PATH = ''
+
+# the path to the sqlite3 database file of user similarities
 USER_SIM_DB_FILE_PATH = ''
